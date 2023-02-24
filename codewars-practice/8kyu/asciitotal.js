c624311b5be436ddc421c4020e41262f9a0e725d
JoshmaxR <joshross0375@gmail.com>
2023-02-23 20:24:27 -0500

    added new codewars challenge

---
+++ codewars-practice/8kyu/asciitotal.js
@@ -0,0 +1,16 @@
+// You'll be given a string, and have to return the sum of all characters as an int. The function should be able to handle all printable ASCII characters.
+
+// Examples:
+
+// uniTotal("a") == 97
+// uniTotal("aaa") == 291
+
+// My solution
+
+function uniTotal(str) {
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+      count += str.charCodeAt(i);
+    }
+    return count;
+  }

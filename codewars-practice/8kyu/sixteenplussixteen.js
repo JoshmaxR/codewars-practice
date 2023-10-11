371e391ed0ca0ccbaad1933fd09784e573321ad1
JoshmaxR <joshross0375@gmail.com>
2023-10-10 21:00:19 -0400

    added new codewars challenge

---
+++ codewars-practice/8kyu/sixteenplussixteen.js
@@ -0,0 +1,28 @@
+// In simple terms, our method does not like the principle of carrying over numbers and just writes down every number it calculates :-)\
+
+// My solution
+
+function add(num1, num2) {
+
+
+    num1 = num1.toString().split("").reverse().join("");
+    num2 = num2.toString().split("").reverse().join("");
+
+ 
+
+    if (num1.length < num2.length) {
+        [num1, num2] = [num2, num1];
+    }
+
+ 
+
+    var returnString = "";
+    for (var i = 0; i < num1.length; i++) {
+        var int1 = parseInt(num1[i]);
+        var int2 = parseInt(num2[i] || 0);
+      
+        returnString = (int1+ int2).toString()+returnString;
+    }
+    
+    return parseInt(returnString);
+}

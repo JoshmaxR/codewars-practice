131a8fab14d49372fea7e7ae643e2ff599f9aa43
JoshmaxR <joshross0375@gmail.com>
2023-10-24 22:25:11 -0400

    added new codewars challenge

---
+++ codewars-practice/8kyu/pellnumbers.js
@@ -0,0 +1,20 @@
+// The Pell sequence is the sequence of integers defined by the initial values
+
+// P(0) = 0, P(1) = 1
+// and the recurrence relation
+
+// P(n) = 2 * P(n-1) + P(n-2)
+// The first few values of P(n) are
+
+// 0, 1, 2, 5, 12, 29, 70, 169, 408, 985, 2378, 5741, 13860, 33461, 80782, 195025, 470832, ...
+// Task
+// Your task is to write a method that returns nth Pell number
+
+// My solution
+
+function pell(n) {
+    n = BigInt(n);
+    let [a,b]=[0n,1n];
+    while ((n=n-1n)>=0n) [a,b]=[b,2n*b+a];
+    return a;
+  }
